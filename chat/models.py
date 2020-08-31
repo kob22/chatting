0e279c418f98881519b7c3a3e06922ae3b49f097
kob22 <kob2222@gmail.com>
2020-08-31 14:43:09 +0200

    topic model

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+class Topic(models.Model):
+    title = models.CharField(max_length=255, validators=[MinLengthValidator(5)])
+    created_at = models.DateTimeField(auto_now_add=True)
--- chat/tests/unittests/test_topic.py
+++ chat/tests/unittests/test_topic.py
@@ -5,41 +5,41 @@ from django.core.exceptions import ValidationError
 
 class TopicModelTest(TestCase):
 
-    def test_default_text(self):
-        topic = Topic()
-        self.assertEqual(topic.title, 'Please type topic text')
+    def test_create_topic(self):
+        topic = Topic(title='Topic title')
+        self.assertEqual(topic.title, 'Topic title')
 
     def test_too_short_title(self):
-        topic = Topic('W'*4)
+        topic = Topic(title='W'*4)
         with self.assertRaises(ValidationError):
             topic.save()
             topic.full_clean()
 
     def test_minimum_length_title(self):
-        topic = Topic('W'*5)
+        topic = Topic(title='W'*5)
         topic.save()
         topic.full_clean() # should not raise ValidationError
-        self.assertEqual(topic.title, 'First')
+        self.assertEqual(topic.title, 'W'*5)
 
     def test_too_long_title(self):
-        topic = Topic('n'*256)
+        topic = Topic(title='n'*256)
 
         with self.assertRaises(ValidationError):
             topic.save()
             topic.full_clean()
 
     def test_max_length_title(self):
-        topic = Topic('n'*255)
+        topic = Topic(title='n'*255)
         topic.save()
         topic.full_clean() # should not raise ValidationError
         self.assertEqual(topic.title, 'n'*255)
 
     def test_allow_duplicate_title(self):
-        first_topic = Topic('Interesting world')
+        first_topic = Topic(title='Interesting world')
         first_topic.save()
         first_topic.full_clean()
 
-        second_topic = Topic('Interesting world')
+        second_topic = Topic(title='Interesting world')
         second_topic.save()
         second_topic.full_clean() # should not raise ValidationError
-        self.assertEqual(second_topic.topic, 'Interesting world')
+        self.assertEqual(second_topic.title, 'Interesting world')
