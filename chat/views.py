1ae06da84aa9bf0ad15f3f6ee286c4a37b50c779
kob22 <kob2222@gmail.com>
2020-09-16 12:19:04 +0200

    topics views

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from . import models
+from . import serializers
 
-# Create your views here.
+
+class TopicViewSet(viewsets.ModelViewSet):
+    queryset = models.Topic.objects.all()
+    serializer_class = serializers.TopicSerializer
--- chatting/urls.py
+++ chatting/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from chat import views as chat_views
+
+router = routers.DefaultRouter()
+router.register(r'topics', chat_views.TopicViewSet, basename='topics')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
