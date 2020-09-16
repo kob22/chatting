3d03b6e0e458ab732813cb06e7b1e821a92f8c05
kob22 <kob2222@gmail.com>
2020-09-16 13:30:57 +0200

    message model

--- chat/models.py
+++ chat/models.py
@@ -5,3 +5,9 @@ from django.core.validators import MinLengthValidator
 class Topic(models.Model):
     title = models.CharField(max_length=255, validators=[MinLengthValidator(5)])
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Message(models.Model):
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
