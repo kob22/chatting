bcfd58b156815948b4dc13b13ef9546d477830ce
kob22 <kob2222@gmail.com>
2020-09-21 18:08:30 +0200

    validation of the immutability of the message topic in model and serializer class

--- chat/models.py
+++ chat/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
-
+from django.core.exceptions import ObjectDoesNotExist
 
 class Topic(models.Model):
     title = models.CharField(max_length=255, validators=[MinLengthValidator(5)])
@@ -11,3 +11,15 @@ class Message(models.Model):
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        if self._state.adding is False:
+            try:
+                msg = Message.objects.get(id=self.id)
+            except ObjectDoesNotExist:
+                msg = None
+            if msg is not None:
+                if msg.topic.id != self.topic.id:
+                    raise(ValueError('You cannot change the topic of the message'))
+
+        super(Message, self).save(*args, **kwargs)
--- chat/serializers.py
+++ chat/serializers.py
@@ -1,5 +1,6 @@
 from .models import Topic, Message
 from rest_framework import serializers
+from django.core.exceptions import ValidationError
 
 
 class TopicSerializer(serializers.ModelSerializer):
@@ -11,4 +12,15 @@ class TopicSerializer(serializers.ModelSerializer):
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
+
         fields = ('id', 'text', 'topic', 'created_at')
+
+    def validate_topic(self, data):
+        instance = self.instance
+
+        if instance is not None:
+            prievous_topic_id = instance.topic.id
+            new_topic_id = data.id
+            if new_topic_id is not None and (prievous_topic_id != new_topic_id):
+                raise ValidationError('Cannot update message topic')
+        return data
